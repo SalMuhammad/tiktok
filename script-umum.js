07ece63b508a3cc0b6207aa15005c8ab8295d42e
Mohammad Faisal <ngomongaja17@gmail.com>
2023-10-08 00:25:09 +0700

    upload first

---
+++ script-umum.js
@@ -0,0 +1,13 @@
+const footer=document.getElementsByTagName('footer')[0];
+
+footer.innerHTML='<div class="footer"><div class="wadah"><nav><a href=""><i class="bi bi-house-door"></i><span>beranda</span></a><a href=""><i class="bi bi-compass"></i><span>temukan</span></a><a href=""><i class="i"></i><span>rekam</span></a><a href=""><i class="bi bi-chat-square-dots"></i><span>kontak masuk</span></a><a href=""><i class="bi bi-person-fill"></i><span class="-fiil">profil</span></a></nav></div></div>';
+
+
+s = document.querySelectorAl('span')
+
+s[1].addEventListener('click', ()=> {
+ document.querySelector('content-nav-tengah').innerHTML = `
+ 
+ 
+ `
+})
